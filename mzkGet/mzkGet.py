dd61adffc2369199c69f95538f73b5ca4b02bfce
SaigoNoo <giorgiodoussis@gmail.com>
2023-03-07 17:12:43 +0100

    7-2023.3#4
    Test if comments is considered as description

--- mzkGet/mzkGet.py
+++ mzkGet/mzkGet.py
@@ -2,13 +2,16 @@ from redbot.core import commands
 from discord import embeds
 
 class MusicBrainz(commands.Cog):
+    """Call some main functions with MusicBrainz"""
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def mzkgetset(self, ctx):
+        """Configure your mzkGet cog"""
         await ctx.send("Setting !")
 
     @commands.command()
     async def mzkget(self, ctx):
+        """Search for a album or a single with title or MusicBrainz ID"""
         await ctx.send("Pong !")
