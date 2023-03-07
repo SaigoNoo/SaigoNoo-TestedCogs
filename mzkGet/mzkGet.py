d1db7ee703813868a400fe3a06873d2b6d346600
SaigoNoo <giorgiodoussis@gmail.com>
2023-03-07 16:59:39 +0100

    7-2023.3#3
    Test if new function will be called + info.json

--- mzkGet/mzkGet.py
+++ mzkGet/mzkGet.py
@@ -1,9 +1,14 @@
 from redbot.core import commands
+from discord import embeds
 
 class MusicBrainz(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def piping(self, ctx):
+    async def mzkgetset(self, ctx):
+        await ctx.send("Setting !")
+
+    @commands.command()
+    async def mzkget(self, ctx):
         await ctx.send("Pong !")
